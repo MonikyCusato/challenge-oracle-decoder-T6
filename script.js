6914d9ace511c5726a2e10e9fbfb6249e48c9331
Moniky Cusato <133261699+MonikyCusato@users.noreply.github.com>
2024-02-20 11:27:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+
+/* Regras Decodificador: 
+"enter" é convertido para "e" 
+"imes" é convertido para "i"
+"ai" é convertido para "a"
+"ober" é convertido para "o"
+"ufat" é convertido para "u"
+Apenas letras minúsculas
+Não permite acentuação     
+*/
+
+function criptografar(){
+	acao();
+}
+
+const entrada = document.getElementById("input-texto");
+const botaoCripto = document.getElementById("btn-cripto");	
+const botaoDescripto = document.getElementById("btn-descripto");
+const inputTexto = document.getElementById("btn-copy");
+const copiarTexto = document.getElementById("msg");
+entrada.focus();
+document.addEventListener("click", function(event){
+	event.preventDefault();
+	let botao = event.target.value;
+
+	if(botao == "criptografar!"){
+		criptografador();
+	} else if(botao == "descriptografar!"){
+		descriptografador();
+	} else if (botao == "copiar"){
+		copiar();
+	}
+});
+
+// function criptografador(){
+// 	let valorTexto = entrada.value;
+// 	let resultado = valorTexto
+// 	   .replace(/e/gi, "enter")
+// 	   .replace(/i/gi, "imes")
+// 	   .replace(/a/gi, "ai")
+// 	   .replace(/o/gi, "ober")
+// 	   .replace(/u/gi, "ufat");
+// 	   copiarTexto.value = resultado;
+// }
+
+// function descriptografador(){
+// 	let textoValor = entrada.value;
+// 	let resultado = textoValor
+// 	   .replace(/enter/gi, "e")
+// 	   .replace(/imes/gi, "i")
+// 	   .replace(/ai/gi, "a")
+// 	   .replace(/ober/gi, "o")
+// 	   .replace(/ufat/gi, "u");
+// 	   copiarTexto.value = resultado;
+// }
+
+
+function criptografador(){
+	let valorTexto = entrada.value;
+	let resultado = valorTexto
+	  .replace(/e/gi, "enter")
+	  .replace(/i/gi, "imes")
+	  .replace(/a/gi, "ai")
+	  .replace(/o/gi, "ober")
+	  .replace(/u/gi, "ufat");
+  
+	esconderImagem();
+  
+	copiarTexto.value = resultado;
+  }
+  
+  function descriptografador(){
+	let textoValor = entrada.value;
+	let resultado = textoValor
+	  .replace(/enter/gi, "e")
+	  .replace(/imes/gi, "i")
+	  .replace(/ai/gi, "a")
+	  .replace(/ober/gi, "o")
+	  .replace(/ufat/gi, "u");
+  
+	esconderImagem();
+  
+	copiarTexto.value = resultado;
+  }
+  
+  function esconderImagem(){
+	document.getElementById("imagemnada").style.display = "none";
+  }
+  
+
+function copiar(){
+	var conteudo = copiarTexto.value;
+
+	if(conteudo != ""){
+		navigator.clipboard
+		  .writeText(conteudo)
+		  .then(() => {
+		  	alert("Texto copiado");
+		  })
+		  .catch((erro) => {
+		  	alert("regra invalida");
+		  });
+	} else {
+	}
+}
+
